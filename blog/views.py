91395da9b50feea109448250976d08b7bbaaa8c8
mehedihasan826 <mehedihasan826@gmail.com>
2021-12-17 02:00:55 +0600

    Final Commit of Blog apps.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from .models import Post
 
@@ -14,3 +15,25 @@ class BlogDetailView(DetailView):
     model = Post
     context_object_name = "PostDetail"
     template_name = 'post_detail.html'
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    context_object_name = "Create_Post"
+    template_name = 'new_post.html'
+    fields = ['title', 'author', 'body']
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    context_object_name = "edit_post"
+    template_name = 'edit_post.html'
+    fields = ['title', 'body']
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    context_object_name = "delete_post"
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('home')
+
